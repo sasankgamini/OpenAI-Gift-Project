b9805d55d7973f3fbac9ae3fb700d99ba8409df3
sasankgamini <sasankgamini@gmail.com>
2023-03-30 19:25:09 -0700

    took in input and used openai api

--- app.py
+++ app.py
@@ -1,21 +1,41 @@
 from flask import Flask, render_template, redirect, request
+import openai
+
+file = open('APIKey.txt', 'r')
+apikey = file.read().strip()
+file.close()
+openai.api_key = apikey
+questions = ["What are the recipient's hobbies or interests?",
+"Is there a specific occasion or event that the gift is for?",
+"What is the recipient's age and gender?",
+"What is the recipient's personal style or taste?",
+"Is there a budget for the gift?"]
 
 app = Flask(__name__) #create object of Flask class
 
-@app.route('/')
+@app.route('/',methods = ["GET","POST"])
 def home():
     if request.method == "GET":
-        questions = ["What are the recipient's hobbies or interests?",
-        "Is there a specific occasion or event that the gift is for?",
-        "What is the recipient's age and gender?",
-        "What is the recipient's personal style or taste?",
-        "Is there a budget for the gift?"]
         return render_template('home.html', questions = questions)
     else:
-        #to get answers: request.form.getlist('nameofinput')
+        # to get answers: request.form.getlist('nameofinput')
         answers = request.form.getlist("answer")
-        print(answers)
-        return redirect('/')
+        messages = []
+        message = f"with the questions and answers for the questions provided, give me 5 seperate perfect gifts that are numbered: questions:{questions} and coressponding answers:{answers}"
+        conversation = {'role':'user', 'content':message}
+        messages.append(conversation)
+        chat = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages = messages)
+        reply = chat.choices[0]
+        # print(reply['message']['content'])
+        perfectgift = reply['message']['content']
+
+        return redirect('/results')
+
+@app.route('/results', methods = ["GET","POST"])
+def results():
+    if request.method == "GET":
+        return render_template("results.html", perfectgift=perfectgift)
+
 
 if __name__ == '__main__': #checking if in actual application and running
     app.run(debug=True)
--- test.py
+++ test.py
@@ -5,11 +5,28 @@ apikey = file.read().strip()
 file.close()
 openai.api_key = apikey
 
-while True:
-    print('enter something')
-    message = input()
-    conversation = {'role':'user', 'content':message}
-    messages.append(conversation)
-    chat = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages = messages)
-    reply = chat.choices[0]
-    print(reply['message']['content'])
+
+# while True:
+#     # print('enter something')
+#     message = input()
+#     conversation = {'role':'user', 'content':message}
+#     messages.append(conversation)
+#     chat = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages = messages)
+#     reply = chat.choices[0]
+#     print(reply['message']['content'])
+
+
+
+questions = ["What are the recipient's hobbies or interests?",
+    "Is there a specific occasion or event that the gift is for?",
+    "What is the recipient's age and gender?",
+    "What is the recipient's personal style or taste?",
+    "Is there a budget for the gift?"]
+answers = ['likes to run and make music', "birthday", "18 male", "not sure", "$200"]
+
+message = f"with the questions and answers for the questions provided, find the perfect gift: questions:{questions} and coressponding answers:{answers}"
+conversation = {'role':'user', 'content':message}
+messages.append(conversation)
+chat = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages = messages)
+reply = chat.choices[0]
+print(reply['message']['content'])
